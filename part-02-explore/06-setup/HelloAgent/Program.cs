2e365a1d0541af23bf5d0ed2f38d0ec31119d8b4
mehmetozkaya <mehmetozkaya@gmail.com>
2026-02-23 11:51:29 +0300

    hello

---
+++ part-02-explore/06-setup/HelloAgent/Program.cs
@@ -0,0 +1,18 @@
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using OpenAI.Chat;
+
+// 1. Define the variables we extracted from Microsoft Foundry
+string endpoint = "https://agents-on-foundry-resource.services.ai.azure.com/";
+string model = "gpt-5-mini";
+
+// 2. Create the Agent using MAF
+AIAgent agent = new AzureOpenAIClient(
+        new Uri(endpoint),
+        new AzureCliCredential())
+    .GetChatClient(model)
+    .AsAIAgent(instructions: "You are a friendly assistant. Keep your answers brief.");
+
+// 3. Invoke the Agent
+Console.WriteLine(await agent.RunAsync("What is the largest city in France?"));
